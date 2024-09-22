314ca04b35926d28dada2115d08c100e676d4424
mourayago <moura.yagoibmec@gmail.com>
2024-09-22 20:58:58 -0300

    Adicionando tipagem na requisição get do hello world

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,9 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
-
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     """hello world usando fast_api
     que irá retorna um json com o conteúdo 'Olá Mundo'"""
--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -6,9 +6,9 @@ from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app) #Arrange orgnização do teste
+    client = TestClient(app)  # Arrange orgnização do teste
 
-    response = client.get('/') #Act, faz algo ação do teste, é o teste em si
+    response = client.get('/')  # Act, faz algo ação do teste, é o teste em si
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
